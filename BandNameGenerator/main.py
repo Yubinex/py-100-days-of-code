31721b03314e513b7aa27eb1528c6821a82fe6d4
Yubinex <janhauck99@gmail.com>
2022-11-30 16:33:02 +0100

    Finished BandNameGenerator Project

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,8 @@
+# Greeting
+print("Welcome to the Band Name Generator!")
+# Ask for city name
+city = input("In which city did you grow up in?\n-> ")
+# Ask for the name of a pet
+pet = input("What's the name of one of your pets?\n-> ")
+# Print formatted string
+print(f"Possible band name: {city} {pet}\n")
