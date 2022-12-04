1a0c4bdc9bb48807c0f2b6128865af68e55fd076
Yubinex <janhauck99@gmail.com>
2022-12-04 04:40:49 +0100

    Day 11 - Finished Blackjack

--- Proj11-Blackjack/main.py
+++ Proj11-Blackjack/main.py
@@ -75,56 +75,83 @@ else:
     print(f"{PLATFORM} is not supported...")
     def clear(): return os.system('cls')
     os.abort
-    
-
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def deal_card():
+    """Return a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
 
 def calculate_score(card_list):
-    score = sum(card_list)
-    if score == 21:
+    """Take a list of cards and return the score calculated from the cards."""
+    if sum(card_list) == 21 and len(card_list) == 2:
         return 0
-    elif 11 in card_list and score > 21:
+    if 11 in card_list and sum(card_list) > 21:
         card_list.remove(11)
         card_list.append(1)
-        return 1
-    else:
-        return score
+    return sum(card_list)
     
     
 def compare(user_score, computer_score):
     if user_score == computer_score:
-        return f"It's a draw!"
+        return "It's a draw!"
     elif user_score == 0:
-        return f"User won with a BLACKJACK!"
+        return "You won with a BLACKJACK!"
     elif computer_score == 0:
-        return f"Computer won with a BLACKJACK!"
-    
+        return "You lost. Computer won with a BLACKJACK!"
     elif user_score > 21:
-        return f"User lost with a score of {user_score}..."
+        return f"You got a bust. {user_score}"
     elif computer_score > 21:
-        return f"Computer lost with a score of {computer_score}..."
-    
+        return f"You won. Computer got a bust. {computer_score}"
+    elif user_score > computer_score:
+        return f"You won with a score of {user_score}!"
     else:
-        if user_score > computer_score:
-            return f"User won with a score of {user_score}!"
-        else:
-            return f"Computer won with a score of {computer_score}!"
+        return f"You lost. Computer won with a score of {computer_score}!"
 
 
 def blackjack():
-    user_cards = []
-    computer_cards = []
     
     print(logo)
-    user_cards.append(deal_card())
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-    computer_cards.append(deal_card())
+    
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+    
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"    Your cards: {user_cards}, current score: {user_score}")
+        print(f"    Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > user_score > 21:
+            is_game_over = True
+        else:
+            user_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_choice == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+        while computer_score != 0 and computer_score < 17:
+            computer_cards.append(deal_card())
+            computer_score = calculate_score(computer_cards)
+        
+        print(f"    Your final hand: {user_cards}, final score: {user_score}")
+        print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
+        print(compare(user_score=user_score, computer_score=computer_score))
+        
+        while input("Play another round? y/n ") == "y":
+            clear()
+            blackjack()
+            
+blackjack()
+"""
+! FIrst solution:        
     while True:
         print(f"Your cards: {user_cards}")
         if calculate_score(user_cards) == 0 or calculate_score(computer_cards) == 0 or calculate_score(user_cards) > 21:
@@ -144,6 +171,4 @@ def blackjack():
         blackjack()
     else:
         print("Program finished...")
-
-
-blackjack()
+"""
