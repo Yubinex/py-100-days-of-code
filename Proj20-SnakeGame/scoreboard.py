e07c5ea3f3fd7b329e6d1466b6f52d8ec29a8848
Yubinex <janhauck99@gmail.com>
2022-12-09 04:10:36 +0100

    Day 24 - SnakeGame high score is saved

--- Proj20-SnakeGame/scoreboard.py
+++ Proj20-SnakeGame/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("./Proj20-SnakeGame/data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.goto(0, 250)
@@ -22,12 +23,10 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("./Proj20-SnakeGame/data.txt", mode="w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
-        
-    """ def game_over(self):
-        self.goto(0, 0)
-        self.write(f"YOU DIED", align=ALIGNMENT, font=FONT) """
 
     def increase_score(self):
         self.score += 1
