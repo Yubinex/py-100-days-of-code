415ece1f8da388395067e54c17fda462bb94a35b
Yubinex <janhauck99@gmail.com>
2022-12-11 04:31:02 +0100

    Day 25 - Finished US_States

---
+++ Proj24-US_States/main.py
@@ -0,0 +1,37 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "Proj24-US_States/blank_states_img.gif"
+screen.setup(height=491, width=725)
+screen.addshape(image)
+turtle.shape(image)
+
+states = pd.read_csv("Proj24-US_States/50_states.csv")
+state_names = states.state.to_list()
+guessed_states = []
+
+score = 0
+while len(guessed_states) < 50:
+    answer_state = str(screen.textinput(
+        title=f"{score}/{len(state_names)} States Correct", prompt="Enter a State's name:")).strip().title()
+
+    if answer_state == "Exit":
+        states_to_learn_list = []
+        for state in state_names:
+            if state not in guessed_states:
+                states_to_learn_list.append(state)
+        data = pd.DataFrame(states_to_learn_list)
+        data.to_csv("Proj24-US_States/states_to_learn.csv")
+        break
+
+    if answer_state in state_names:
+        guessed_states.append(answer_state)
+        score += 1
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = states[states.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
