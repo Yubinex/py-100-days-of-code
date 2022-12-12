46785f677dc22f0a40c3bffaf269149c9734592d
Yubinex <janhauck99@gmail.com>
2022-12-12 14:38:00 +0100

    Day 30 - Finished ErrorHandling Exercises

--- Proj25-NATO_Alphabet/main.py
+++ Proj25-NATO_Alphabet/main.py
@@ -3,6 +3,14 @@ import pandas
 nato_df = pandas.read_csv("Proj25-NATO_Alphabet/nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-user_input = input("Enter a word: ").strip().upper()
-phonetic_list = [nato_dict[letter] for letter in user_input]
-print(phonetic_list)
+def generate_phonetic():
+    user_input = input("Enter a word: ").strip().upper()
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+generate_phonetic()
