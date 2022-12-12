2a30e200878492c4dc10a23f9e34144ac8cd1410
Yubinex <janhauck99@gmail.com>
2022-12-12 17:14:32 +0100

    Day 30 - Finished Improving PasswordManager

--- Proj28-PasswordManager/main.py
+++ Proj28-PasswordManager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -9,7 +10,7 @@ import pyperclip
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -34,25 +35,58 @@ def save():
     website = website_entry.get()
     email_username = email_username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email_username,
+            "password": password,
+        },
+    }
 
     if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
         messagebox.showerror(
             title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(
-            title=website, message=f"These are the details entered:\nEmail: {email_username}\nPassword: {password}\nIs it ok to save?")
+        try:
+            with open("Proj28-PasswordManager/data.json", "r") as f:
+                # reading old data
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("Proj28-PasswordManager/data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            # updating old data
+            data.update(new_data)
 
-        if is_ok:
-            with open("Proj28-PasswordManager/data.txt", "a") as f:
-                f.write(f"{website} | {email_username} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-                website_entry.focus()
+            with open("Proj28-PasswordManager/data.json", "w") as f:
+                # saving updated data
+                json.dump(data, f, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            website_entry.focus()
+
+
+# ---------------------------- SEARCH ------------------------------- #
+
+
+def search():
+    website = website_entry.get()
+    try:
+        with open("Proj28-PasswordManager/data.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"No entry for website \"{website}\" found.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -77,8 +111,8 @@ password.grid(column=0, row=3)
 
 # ---------------------------- ENTRIES ---------------------------- #
 
-website_entry = Entry(width=50)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=34)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_username_entry = Entry(width=50)
@@ -89,6 +123,10 @@ password_entry = Entry(width=34)
 password_entry.grid(column=1, row=3)
 
 # ---------------------------- BUTTONS ---------------------------- #
+
+search_button = Button(text="Search", command=search,
+                       highlightthickness=0, padx=0, pady=0, width=15)
+search_button.grid(column=2, row=1)
 
 password_button = Button(text="Generate Password", command=generate_password,
                          highlightthickness=0, padx=0, pady=0)
